2b77b3247a2d564e781931009cb7bc778dcf7958
Sophie <Sophie-de-Lalalie@users.noreply.github.com>
2020-11-23 00:58:52 +0100

    initial

---
+++ README.md
@@ -0,0 +1,29 @@
+## Star Citizen - Fleet Info
+
+This code injects a new sub page in the account section of https://robertsspaceindustries.com
+
+On this page, all vehicles contained in packages are displayed with some info.
+
+![example](example.jpg)
+
+### Status
+
+WIP prototype, so it's quite dirty. Expect bugs.
+
+### Howto use
+
+There is no dedicated browser extension yet.
+
+Best way is to use any script injection plugin for your browser and inject the 
+contents of fleetInfo.js on "https://robertsspaceindustries.com/account" and its sub pages.
+
+I use [https://addons.mozilla.org/de/firefox/addon/codeinjector](https://addons.mozilla.org/de/firefox/addon/codeinjector)
+([https://github.com/Lor-Saba/Code-Injector](https://github.com/Lor-Saba/Code-Injector))
+
+![example](example_use.jpg)
+
+### Acknowledgment
+
+I borrowed some code from [https://github.com/Ioni14/fleet-manager-extension](https://github.com/Ioni14/fleet-manager-extension). 
+
+Thanks to them.
---
+++ fleetInfo.js
@@ -0,0 +1,276 @@
+$(function () {
+    'use strict';
+    let pledges = [];
+    let skins = [];
+
+    const INSURANCE_TYPE_LTI = 'lti';
+    const INSURANCE_TYPE_IAE = 'iae';
+    const INSURANCE_TYPE_MONTHLY = 'monthly';
+
+    const _manufacturerShortMap = {
+        'ANVL': ['Anvil Aerospace', 'Anvil'],
+        'AEGS': ['Aegis Dynamics', 'Aegis'],
+        'AOPOA': ['Aopoa'],
+        'ARGO': ['ARGO Astronautics', 'ARGO'],
+        'BANU': ['Banu'],
+        'CNOU': ['Consolidated Outland', 'Consolidated', 'C.O.'],
+        'CRSD': ['Crusader Industries', 'Crusader'],
+        'DRAK': ['Drake Interplanetary', 'Drake'],
+        'ESPERIA': ['Esperia'],
+        'GRIN': ['Greycat Industrial', 'Greycat'],
+        'KRGR': ['Kruger Intergalactic', 'Kruger'],
+        'MISC': ['MISC', 'Musashi Industrial & Starflight Concern', 'M.I.S.C.'],
+        'ORIG': ['Origin Jumpworks', 'Origin Jumpworks GmbH', 'Origin'],
+        'RSI': ['Roberts Space Industries', 'RSI', 'R.S.I.'],
+        'TMBL': ['Tumbril'],
+        'VANDUUL': ['Vanduul'],
+        'XIAN': ['Xi\'an'],
+    };
+
+    const createPledge = function($pledge, name, manufacturer, insuranceType, insuranceDuration, gamePackage, image) {
+        if (image == undefined) {
+            image = $('div.image', $pledge).css('background-image');
+        }
+
+        let pledge = {
+            id: $('.js-pledge-id', $pledge).val(),
+            insurance_type: insuranceType,
+            insurance_duration: insuranceDuration,
+            package_id: $('.js-pledge-id', $pledge).val(),
+            pledge: $('.js-pledge-name', $pledge).val(),
+            image: image,
+            gamePackage: gamePackage
+        };
+
+        let manufacturerNames = _manufacturerShortMap[manufacturer] || [manufacturer];
+        pledge.manufacturer = manufacturerNames[0];
+
+        $.each(manufacturerNames, function(mi, manufacturerName) {
+            name = name.replace(new RegExp(manufacturerName, 'gi'), '');
+        });
+
+        name = name.replace(/\-/gi, '').trim();
+        pledge.name = name;
+        pledge.shortName = name.split(' ')[0];
+
+        return pledge;
+    };
+
+    const processPledges = function(body) {
+        $('.list-items li', body).each((index, el) => {
+            const $pledge = $(el);
+
+            // game package?
+            let gamePackage = false;
+            $pledge.find('.without-images .item .title').each((i, elBonus) => {
+                const bonus = $(elBonus).text().trim();
+                if (/Star\sCitizen\sDigital\sDownload/i.test(bonus)) {
+                    gamePackage = true;
+                }
+            });
+
+            let insuranceType = null;
+            let insuranceDuration = null;
+
+            // compute the Insurance type of the pledge
+            $pledge.find('.without-images .item .title').each((i, elBonus) => {
+                const bonus = $(elBonus).text().trim();
+
+                if (/Lifetime\s+Insurance/i.test(bonus)) {
+                    insuranceType = INSURANCE_TYPE_LTI;
+                } else if (/IAE\s+Insurance/i.test(bonus)) {
+                    insuranceType = INSURANCE_TYPE_IAE;
+                } else {
+                    const insuranceRegexResult = /(\d+)(\s+|-)Months?\s+Insurance/i.exec(bonus);
+
+                    if (insuranceRegexResult !== null && insuranceRegexResult[1]) {
+                        insuranceDuration = parseInt(insuranceRegexResult[1]);
+
+                        if (insuranceDuration > 0) {
+                            insuranceType = INSURANCE_TYPE_MONTHLY;
+                        }
+                    }
+                }
+            });
+
+            // browse the ships that are in "Also contains" part
+            $pledge.find('.without-images .item .title').each((i, elBonus) => {
+                const bonus = $(elBonus).text().trim();
+
+                if (/Origin\s+G12[ar]/i.test(bonus)) {
+                    const shipInfoRegexResult = /(Origin)\s+(G12[ar])/i.exec(bonus);
+                    const pledge = createPledge(
+                        $pledge,
+                        shipInfoRegexResult[2],
+                        shipInfoRegexResult[1],
+                        insuranceType,
+                        insuranceDuration,
+                        gamePackage
+                    );
+
+                    pledges.push(pledge);
+                }
+            });
+
+            // browse the Ship items
+            $('.items .item', $pledge).each((indexItem, elItem) => {
+                const $item = $(elItem);
+
+                // skins
+                let $shipInfo = $item.find('.kind:contains(Skin)').parent();
+                if ($shipInfo.length !== 0) {
+                    skins.push($('.title', $shipInfo).text());
+                    return;
+                }
+
+                // special cases
+                $shipInfo = $item.find('.kind:contains(Hangar decoration)').parent();
+                if ($shipInfo.length !== 0) {
+                    if ($('.liner', $shipInfo).text().indexOf('Greycat Industrial') !== -1
+                        && $('.title', $shipInfo).text().indexOf('Greycat PTV') !== -1) {
+
+                        // Found the ship "Greycat PTV" from "Greycat Industrial"
+                        const pledge = createPledge(
+                            $pledge,
+                            $('.title', $shipInfo).text(),
+                            $('.liner span', $shipInfo).text(),
+                            insuranceType,
+                            insuranceDuration,
+                            gamePackage
+                        );
+
+                        pledges.push(pledge);
+                    }
+                    return;
+                }
+
+                $shipInfo = $item.find('.kind:contains(Ship)').parent();
+                if ($shipInfo.length === 0) {
+                    return;
+                }
+
+                const pledge = createPledge(
+                    $pledge,
+                    $('.title', $shipInfo).text(),
+                    $('.liner span', $shipInfo).text(),
+                    insuranceType,
+                    insuranceDuration,
+                    gamePackage,
+                    $('div.image', $item).css('background-image')
+                );
+
+                pledges.push(pledge);
+            });
+        })
+    };
+
+    const renderFleet = function(fleetList) {
+        $.each(pledges, function(index, pledge) {
+            console.log(pledge);
+
+            let imageBox = $('<div></div>');
+            imageBox.css('background-image', pledge.image);
+            imageBox.css('background-size', 'cover');
+            imageBox.css('background-position', 'center');
+            imageBox.css('background-repeat', 'no-repeat');
+            imageBox.css('height', '100px');
+
+            let infoBox = $('<ul></ul>');
+            let infoTemplate = $('<li></li>');
+            infoTemplate.css('border-top', '1px solid rgb(29, 45, 66)');
+            infoTemplate.css('padding', '3px 0');
+
+            let nameBox = infoTemplate.clone();
+            nameBox.css('border-top', 'none');
+            nameBox.css('padding', '3px 0 0 0');
+            nameBox.css('font-size', '1.2em');
+            nameBox.css('color', '#fff');
+            nameBox.text(pledge.name);
+            infoBox.append(nameBox);
+
+            let manuBox = infoTemplate.clone();
+            manuBox.css('border-top', 'none');
+            manuBox.css('border-bottom', '3px solid rgb(29, 45, 66)');
+            manuBox.css('padding', '0 0 6px 0');
+            manuBox.css('font-size', '0.8em');
+            manuBox.text(pledge.manufacturer);
+            infoBox.append(manuBox);
+
+            if (pledge.gamePackage) {
+                infoBox.append(infoTemplate.clone().text("Game Package"));
+            }
+
+            if (pledge.insurance_type == INSURANCE_TYPE_LTI) {
+                infoBox.append(infoTemplate.clone().text("Life Time Insurance"));
+            } else {
+                infoBox.append(infoTemplate.clone().text(pledge.insurance_duration + " Month/s Insurance"));
+            }
+
+            $.each(skins, function(skinIndex, skinName) {
+                if (skinName.search(pledge.shortName) != -1) {
+                    infoBox.append(infoTemplate.clone().text(skinName));
+                }
+            });
+
+            let inner = $('<div></div>');
+            inner.css('background-color', 'rgba(23,29,37,0.5)');
+            inner.css('padding', '10px');
+            inner.css('margin', '5px');
+
+            inner.append(imageBox);
+            inner.append(infoBox);
+
+            let newEntry = $('<div></div>');
+            newEntry.css('width', '20%');
+            newEntry.css('position', 'relative');
+            newEntry.append(inner);
+            fleetList.append(newEntry);
+        });
+    };
+
+    const loadData = function(page, callback) {
+        const url = '/account/pledges?page=' + page;
+        const $page = $('<div>');
+        $page.load(url + ' .page-wrapper', function (response, status) {
+            if ($('.list-items .empy-list', this).length > 0) return callback();
+            processPledges(this);
+            loadData(page+1, callback);
+        });
+    };
+
+    const renderFleetPage = function() {
+        $('div.sidenav ul li').removeClass('active');
+        $('.showFleetsButton').addClass('active');
+        let top = $('<div class="top"></div>');
+        $('.inner-content').empty().css('box-sizing', 'inherit').append(top);
+        let title = $('<h2 class="title">MY FLEET</h2>');
+        top.append(title);
+        let sep = $('<div class="separator"></div>');
+        top.append(sep);
+
+        let fleetList = $('<div class="fleetList"></div>');
+        fleetList.css('display', 'flex');
+        fleetList.css('flex-wrap', 'wrap');
+        fleetList.css('color', '#bdced4');
+        fleetList.css('font-size', '0.6em');
+        $('.inner-content').append(fleetList);
+
+        loadData(1, function(){
+            renderFleet(fleetList);
+        });
+    };
+
+    let nav = $('div.sidenav ul li');
+    if (nav.length > 0) {
+        let newNav = $(nav[0]).clone();
+        newNav.removeClass('active');
+        newNav.addClass('showFleetsButton');
+        newNav.find('.bg').text('MY FLEET');
+        newNav.find('a').attr('href', 'javascript:void(0)');
+        newNav.find('a').click(renderFleetPage);
+        $('div.sidenav ul').before(newNav);
+    }
+});
+
+
+
